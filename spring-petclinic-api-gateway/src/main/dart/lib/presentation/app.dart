e96dd50a1f3f72480f2729cf950fff0a8ecc942d
Saghir <ahmedalsaghirxyz@gmail.com>
2024-01-01 01:50:12 +0200

    stabling build process for local and docker-compose flows

--- spring-petclinic-api-gateway/src/main/dart/lib/presentation/app.dart
+++ spring-petclinic-api-gateway/src/main/dart/lib/presentation/app.dart
@@ -5,19 +5,17 @@ import 'package:flutter/material.dart';
 /// rendering logic goes here. Example of that is loading config
 /// from yaml file and make sure it is loaded on startup
 class AppScreen extends StatefulWidget {
-    final Widget tabContent;
-  
-    const AppScreen({Key? key, required this.tabContent}) : super(key: key);
-  
-    @override
-    State<AppScreen> createState() => _AppScreenState();
+  final Widget tabContent;
+
+  const AppScreen({super.key, required this.tabContent});
+
+  @override
+  State<AppScreen> createState() => _AppScreenState();
+}
+
+class _AppScreenState extends State<AppScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return widget.tabContent;
   }
-  
-  class _AppScreenState extends State<AppScreen> {
-  
-    @override
-    Widget build(BuildContext context) {
-      return widget.tabContent;
-    }
-  }
-  
+}
